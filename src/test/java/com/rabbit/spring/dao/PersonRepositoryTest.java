d1de6c3f19d076e24eb0b8ee26704e3f55d52b0f
guoyankui <yankuiguo@gmail.com>
2016-09-06 21:57:22 +0800

    add test jpa

--- src/test/java/com/rabbit/spring/dao/PersonRepositoryTest.java
+++ src/test/java/com/rabbit/spring/dao/PersonRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.rabbit.spring.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +31,19 @@ public class PersonRepositoryTest {
 	public void testFindOne(){
 		Person person = personRepository.findOne(1L);
 		System.out.println(JSONObject.toJSONString(person));
+	}
+	
+	@Test
+	public void testBatchSave(){
+		List<Person> persons = new ArrayList<Person>();
+		for(int i=0; i<100; i++){
+			Person person = new Person();
+			person.setAge(i);
+			person.setMan(i%2 == 0);
+			person.setName("zhangsan" + i);
+			persons.add(person);
+		}
+		personRepository.save(persons);
 	}
 
 }
